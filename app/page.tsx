e7023b013f3c232aa2f7366005e395d517250ee8
leejunsibal <118183460+MicKoreaYoutube@users.noreply.github.com>
2024-07-23 13:13:20 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function IndexPage() {
       <div className="flex flex-col gap-2 items-center">
         <Ban className="text-red-500" size={400}/>
         <h1 className="text-3xl md:text-5xl font-extrabold leading-tight tracking-tighter font-KBO-Dia-Gothic_bold">
-          상명중 1학년 1반 건의함은 <br />2024년 2월 6일부로 폐쇄되었습니다.
+          상명중 2학년 1반 건의함은 <br />2024년 7월 23일부로 폐쇄되었습니다.
         </h1>
         <p className="text-lg md:text-2xl text-muted-foreground font-SUITE-Regular">
           이용해주셔서 감사합니다.
